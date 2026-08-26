19129102510c2763962d65ad6d696a51633aba07
ayotidapo <otidapson@gmail.com>
2026-08-26 09:47:11 +0100

    clear all console log

--- src/api/fetch.ts
+++ src/api/fetch.ts
@@ -1,16 +1,12 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { apiBaseConfig, stripEmpty } from '@/utils/helper';
 
-// type TbaseUrl =
-// 	| 'https://content.guardianapis.com/search'
-// 	| 'https://api.nytimes.com/svc/search/v2/articlesearch.json'
-// 	| 'https://newsapi.org/v2/everything';
 type Source = 'guardian' | 'new_times' | 'news_api';
 const Fetch = async (url: Source, query: Record<string, any> = {}) => {
 	const apiBase = apiBaseConfig[url];
 	const uri = apiBase.uri;
 	const strippedQuery = stripEmpty(query);
-	console.log({ apiBase, strippedQuery, m: uri });
+
 	const params = new URLSearchParams({
 		[apiBase.apiParams]: apiBase?.value,
 		...strippedQuery,
